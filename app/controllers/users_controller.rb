2598ea9a6e7e37058a0dc5ccc51d28eb71b087f6
Lee jaeyeon <nicola@overpl.us>
2014-05-27 06:49:22 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 	def update
+		User.where(sid:params[:user][:sid]).map(&:destroy)
 		current_user.sid=params[:user][:sid]
 		current_user.save!
-		#raise 'dfr'
 		redirect_to :root
 	end
 end
